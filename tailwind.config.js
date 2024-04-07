50046ae11c75d28ff4a0400a66e1432e418c73d9
Richard Melkonian <r.v.melkonian@gmail.com>
2024-04-07 18:50:13 +0200

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    `./src/pages/**/*.{js,jsx,ts,tsx}`,
-    `./src/components/**/*.{js,jsx,ts,tsx}`,
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        stripes:
+          'linear-gradient(135deg, #fff 0%, #fff 5%, transparent 5%, transparent 50%, #fff 50%, #fff 55%, transparent 55%, transparent 100%)',
+      },
+      backgroundSize: {
+        stripes: '20px 20px',
+      },
+      keyframes: {
+        stripes1: {
+          '0%': { backgroundPosition: '0 0' },
+          '100%': { backgroundPosition: '0 100vh' },
+        },
+        stripes2: {
+          '0%': { backgroundPosition: '0 0' },
+          '100%': { backgroundPosition: '0 200vh' },
+        },
+      },
+      animation: {
+        stripes1: 'stripes1 8s linear infinite',
+        stripes2: 'stripes2 12s linear infinite',
+      },
+    },
   },
   plugins: [],
-}
+};
